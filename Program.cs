c5861dcbcae7e47aa3ed62b640b708b34d1bee4a
s3Xar <cesarcea1994@gmail.com>
2026-04-13 23:03:56 -0400

    proyecto creado. boceto de clase PlantaExotica con el método Vender.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class PlantaExotica
+{
+    private string nombre;
+    private float precio;
+    private int stock;
+    public PlantaExotica(string nombrePlanta, float precioPlanta, int stockPlanta)
+    {
+        nombre = nombrePlanta; 
+        precio = precioPlanta;
+        stock = stockPlanta;
+    }
+    //falta agregar el get/set para que el número no sea negativo en precio y stock.
+    public void Vender()
+    {
+        if (stock > 0)
+        {
+            stock --;
+            Console.WriteLine("Venta exitosa.");
+        }
+        else
+        {
+            Console.WriteLine("No hay stock suficiente.");
+        }
+    }
+}
